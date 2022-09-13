3b337273931f3f8eade6af14ac47cffd03ff931f
brs14shn <brsshn292929@gmail.com>
2022-09-13 20:54:49 +0300

    apı added

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,4 +1,34 @@
 from django.shortcuts import render
+from decouple import config
+import requests # İstek(get) isteği
+from pprint import pprint # gelen veriyi daha okunaklı hale getirdik
+
+
+
+
+  
 
 def index(request):
-    return render(request, 'weatherapp/index.html')
+    API_KEY = config('API_KEY')
+    city = "Bursa"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+
+    response= requests.get(url)
+    #print(response)
+    content = response.json() #? dictionary yapısına çevirmiş olduk
+    #pprint(content)
+    pprint(content["name"])
+    pprint(content['main']['temp']),
+    pprint(content['weather'][0]['description'])
+    pprint(content['weather'][0]['icon'])
+
+    context={
+        "city":content["name"],
+        "temp":content['main']['temp'],
+        "icon":content['weather'][0]['icon'],
+        "desc":content['weather'][0]['description']
+
+    }
+    return render(request, 'weatherapp/index.html',context)
+
+    
