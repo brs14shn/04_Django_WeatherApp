aca391d49b0b0d141320517bb4c08d036d58925f
brs14shn <brsshn292929@gmail.com>
2022-09-13 21:36:26 +0300

    modes CityClass added

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from decouple import config
 import requests # İstek(get) isteği
 from pprint import pprint # gelen veriyi daha okunaklı hale getirdik
+from django.contrib import messages
 
+from .models import City
 
 
 
@@ -10,17 +12,26 @@ from pprint import pprint # gelen veriyi daha okunaklı hale getirdik
 
 def index(request):
     API_KEY = config('API_KEY')
+    user_city=request.POST.get("name")
     city = "Bursa"
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    if user_city:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={user_city}&appid={API_KEY}&units=metric"
+        response= requests.get(url)
+        print(response.ok) #! True
 
-    response= requests.get(url)
-    #print(response)
-    content = response.json() #? dictionary yapısına çevirmiş olduk
-    #pprint(content)
-    pprint(content["name"])
-    pprint(content['main']['temp']),
-    pprint(content['weather'][0]['description'])
-    pprint(content['weather'][0]['icon'])
+        if response.ok:
+           content = response.json() #? dictionary yapısına çevirmiş olduk
+           r_city=content["name"]
+           if City.objects.filter(name=r_city):
+                messages.warning(request, 'City already exists!')
+           else:
+                City.objects.create(name=r_city)
+                messages.success(request, 'City created!')
+
+        else:
+             messages.warning(request, 'There is no city!')
+
+
 
     context={
         "city":content["name"],
